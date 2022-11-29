add0e1e40f00fd5609e1e6346e85902ebfef8c5f
Michał Manios <michal.manios83@gmail.com>
2022-11-29 22:13:18 +0100

    little refactoring in NumbersSorted and NumbersToSort class

--- exchange-rate/src/main/java/pl/xcodesoftware/exchangerate/numbersort/dto/NumbersToSort.java
+++ exchange-rate/src/main/java/pl/xcodesoftware/exchangerate/numbersort/dto/NumbersToSort.java
@@ -1,17 +1,23 @@
 package pl.xcodesoftware.exchangerate.numbersort.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class NumbersToSort extends NumbersSorted{
 
     @NotNull
     private OrderType order;
+
+    public NumbersToSort(List<Integer> numbers, @NotNull OrderType order) {
+        super(numbers);
+        this.order = order;
+    }
+
 }
